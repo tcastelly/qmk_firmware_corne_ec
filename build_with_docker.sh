25c2b94bdffcaa02165db48414ae5d5fd43c4064
Thomas CASTELLY <thomas@castelly.fr>
2023-04-26 17:47:30 +0200

    try fix x86 build

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git submodule update --init --recursive
+# git submodule update --init --recursive
 
-r=`docker image inspect qmkfm/qmk_cli 2>/dev/null`
+r=`docker image inspect qmkfm/qmk_cli:corne_ec 2>/dev/null`
 
 if [ "$r" == [] ]; then
-    docker build -t qmkfm/qmk_cli .
+    docker build -t qmkfm/qmk_cli:corne_ec .
 fi
 
-docker run -v $PWD:/qmk_firmware -v $PWD/.build:/qmk_firmware/.build -it qmkfm/qmk_cli make sekigon/crkbd_ec:tcy:uf2
+docker run -v $PWD:/qmk_firmware -v $PWD/.build:/qmk_firmware/.build -it qmkfm/qmk_cli:corne_ec make sekigon/crkbd_ec:tcy:uf2
