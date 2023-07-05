2c116902c79165864340af7745d2393d574bc2bd
Thomas CASTELLY <thomas@castelly.fr>
2023-07-05 13:50:44 +0200

    fix build

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ RUN apt-get update -y && \
     apt install -y libstdc++-arm-none-eabi-newlib
 
 VOLUME /qmk_firmware
+
 WORKDIR /qmk_firmware
+
+RUN git config --global --add safe.directory '*'
 
 CMD qmk compile -kb all -km default
--- build_with_docker.sh
+++ build_with_docker.sh
@@ -8,4 +8,4 @@ if [ "$r" == [] ]; then
     docker build -t qmkfm/qmk_cli:corne_ec .
 fi
 
-docker run -v $PWD:/qmk_firmware -v $PWD/.build:/qmk_firmware/.build -it qmkfm/qmk_cli:corne_ec make sekigon/crkbd_ec:tcy:uf2
+docker run --rm -v $PWD:/qmk_firmware -v $PWD/.build:/qmk_firmware/.build -it qmkfm/qmk_cli:corne_ec make sekigon/crkbd_ec:tcy:uf2
