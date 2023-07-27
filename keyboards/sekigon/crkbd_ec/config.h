f1c6d66cc1121086ad2e3094deb8ec1ece18698d
Thomas CASTELLY <thomas@castelly.fr>
2023-07-27 23:34:40 +0200

    use right as master and disable logs

--- keyboards/sekigon/crkbd_ec/config.h
+++ keyboards/sekigon/crkbd_ec/config.h
@@ -63,7 +63,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define SOFT_SERIAL_PIN 1
 
-// #define MASTER_RIGHT
+#define MASTER_RIGHT
 // #define EE_HANDS
 
 #define RGB_DI_PIN 7
@@ -158,10 +158,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* disable debug print */
-//#define NO_DEBUG
+#define NO_DEBUG
 
 /* disable print */
-//#define NO_PRINT
+#define NO_PRINT
 
 /* disable action features */
 //#define NO_ACTION_LAYER
